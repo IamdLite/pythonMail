c0e560fca598ec93d193097900fde8139d457151
The Lite <infinity@lite.eOfbit>
2022-10-06 00:41:57 +0100

    base project completed

---
+++ README.md
@@ -0,0 +1,23 @@
+# pythonMail
+pythonMail is an intermediate program written in Python that let's you send automated happy birthday wishes to your loved ones in due time.
+
+# Note | Prerequisites to run this program
+
+Before running this program ensure that:
+Step 1 - You create a random email to test the program
+
+Step 2 - In the "manage account" settings of the email, go to "Security" and actived the option "Allow insecure access from apps"
+
+[?] The purpose of step 2 is to allow the smtp library to access the newly created account at the time the email is sent
+
+Step 3 - The files;
+- birthdays.csv: shhoul contain the emails and birthdays of your loved ones
+- letters_template / letter_3.txt: a sample of the birthday wish to be sent through email to them
+
+Step 4 - Follow the comments in the code for appropriate guidance,
+
+## Concepts Practised
+
+- Hands-on the smtp library
+- Reading and writing to files using the "open with()" method
+- Data collection and manipulation using the Pandas Library
--- main.py
+++ main.py
@@ -1,13 +1,40 @@
 import smtplib
+import datetime as dt
+import pandas
 
-my_email = "diffouofopa.esdras@ictuniversity.edu.cm"
-my_password = "Fopa@ictu2021"
+# Enter the newly created random email here
+MY_EMAIL = "example@gmal.com" 
 
-with smtplib.SMTP("smtp.gmail.com",  587) as connection:
-    connection.starttls()
-    connection.login(user = my_email, password = my_password )
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs="langmi.prosper@ictuniversity.edu.cm",
-        msg="subject:Lp Studio\n\nExpressing emotions through media"
-    )
+# Enter the password of the email
+MY_PASSWORD = "password"
+
+PLACE_HOLDER = "[name]"
+# The placeholder will be replaced by the various names to customize the wishes
+list_birthdays = []
+
+birthdays = pandas.read_csv("./birthdays.csv")
+list_birthdays = birthdays.to_dict(orient="records")
+mailing_day = dt.datetime.now().day
+mailing_month = dt.datetime.now().month
+
+def get_message(name):
+    with open("./letter_templates/letter_3.txt") as file:
+        mail = file.read()
+        new_mail = mail.replace(PLACE_HOLDER, f"{name}")
+        return new_mail
+          
+for birthday in list_birthdays:
+    name = birthday["name"]
+    name = dt.datetime(year=birthday["year"], month=birthday["month"], day=birthday["day"])
+    
+    if name.date().day == mailing_day and name.date().month == mailing_month:
+        custom_mail = get_message(birthday["name"])  
+        with smtplib.SMTP("smtp.gmail.com",  587) as connection:
+            connection.starttls()
+            connection.login(user = MY_EMAIL, password = MY_PASSWORD )
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs=birthday["email"],
+                msg=f"subject:Happy birthday\n\n{custom_mail}"
+            )
+
